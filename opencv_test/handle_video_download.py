39323f02efbb71733f2a121dcf54b8fa2fcc2cd6
FKgk <rhkd865@naver.com>
2019-07-02 17:34:04 +0900

    영상 저장

---
+++ opencv_test/handle_video_download.py
@@ -0,0 +1,29 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter("test.avi", fourcc, 25.0, (640, 480))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        # 이미지 반전, 0:상하, 1:좌우
+        # frame = cv2.flip(frame, 0)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        out.write(frame)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+print("end")
